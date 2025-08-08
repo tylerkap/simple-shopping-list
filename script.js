4a28bfa90831c2e38d44b1745b00fbac46326171
Tyler Kap <tylerkap55@gmail.com>
2025-08-08 14:38:59 -0600

    Implemented submit and delete buttons

---
+++ script.js
@@ -0,0 +1,33 @@
+let submitButton = document.querySelector("#btn");
+let input = document.querySelector("#list-input");
+let unorderdList = document.querySelector("#list");
+
+
+
+submitButton.addEventListener('click', () => {
+
+    alert("Is this working?");
+    
+    let div = document.createElement("div");
+    let listItem = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    div.classList.add("item");
+    listItem.classList.add("list-item");
+    listItem.textContent = input.value;
+    deleteButton.classList.add("delete-btn");
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener('click', () => {
+        alert("DOES THIS WORK?");
+        div.remove();
+    });
+
+    div.appendChild(listItem);
+    div.appendChild(deleteButton);
+
+    unorderdList.appendChild(div);
+    
+
+    input.value = "";
+    input.focus();
+});
